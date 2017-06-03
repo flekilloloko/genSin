6cb11bd7e26664a7e447f3e16d7f21ddf6eb6b49
Mom is life <lamagiadelalocura@hotmail.com>
2017-06-02 22:08:31 -0300

    Comunicacion serie

--- gen_sin.ino
+++ gen_sin.ino
@@ -2,7 +2,7 @@ int W_CLK = 2;
 int FQ_UP = 3;
 int RESET = 4;
 
-int D0 = 5, D1 = 6, D2 = 7, D3 = 8, D4 = 9, D5 = 10, D6 = 11, D7 = 12;
+int Dout = 5;
 int palabra, resto, leer, i;
 long frec;
 int lectura;
@@ -15,7 +15,7 @@ void setup() {
   pinMode(W_CLK, OUTPUT);
   pinMode(FQ_UP, OUTPUT);
   pinMode(RESET, OUTPUT);
-  pinMode(D0, OUTPUT); pinMode(D1, OUTPUT); pinMode(D2, OUTPUT); pinMode(D3, OUTPUT); pinMode(D4, OUTPUT); pinMode(D5, OUTPUT); pinMode(D6, OUTPUT); pinMode(D7, OUTPUT);
+  pinMode(Dout, OUTPUT);// pinMode(D1, OUTPUT); pinMode(D2, OUTPUT); pinMode(D3, OUTPUT); pinMode(D4, OUTPUT); pinMode(D5, OUTPUT); pinMode(D6, OUTPUT); pinMode(D7, OUTPUT);
  // pinMode(POTE1, OUTPUT); pinMode(POTE2, OUTPUT); pinMode(POTE3, OUTPUT); pinMode(POTE4, OUTPUT); pinMode(POTE5, OUTPUT); 
   //      x00K                     x0K                    xK                       x00			           xx
  // pinMode(SW, INPUT);
@@ -45,7 +45,6 @@ void loop() {
 	  if((lectura>=102.4*i-51.2)&&(lectura<102.4*(i+1)-51.2))   // Se busca
 		  frec += (100L*i / 0.0291);
   
-  /*
   delay(retardo);
   lectura = analogRead(POTE4);
   for (i=0;i<10;i++) 
@@ -63,15 +62,13 @@ void loop() {
   for (i=0;i<10;i++) 
     if((lectura>=102.4*i-51.2)&&(lectura<102.4*(i+1)-51.2))   // Se busca
 		  frec += (100000L*i / 0.0291);
-  */
+  
   frecuency(frec);
   delay(100);
 }
 
 void frecuency(long freq){
-  palabra = 0;
   digitalWrite(RESET, HIGH); delay(1); digitalWrite(RESET, LOW);
-  cargarDatos(palabra); //word 0    =   Fase y control
   palabra = freq / pow(2,24);
   cargarDatos(palabra); //word 1	=	Frec-b31 / Frec-b24
   palabra = (freq & (long)(pow(2,24)-1) ) / pow(2,16);		  //-65535
@@ -80,19 +77,21 @@ void frecuency(long freq){
   cargarDatos(palabra); //word 3	=	Frec-b15 / Frec-b8
   palabra = (freq & (long)(pow(2,8)-1)   ) ;									// b5 = 0.9312 Hz
   cargarDatos(palabra); //word 4	=	Frec-b7  / Frec-b0      // LSB = 0.0291 Hz
+  palabra = 0;
+  cargarDatos(palabra); //word 0    =   Fase y control
   digitalWrite(FQ_UP, HIGH); digitalWrite(FQ_UP, LOW); 
 }
 
 void cargarDatos(int data){
-  digitalWrite(D0, bitRead(data,0));
-  digitalWrite(D1, bitRead(data,1));
-  digitalWrite(D2, bitRead(data,2));
-  digitalWrite(D3, bitRead(data,3));
-  digitalWrite(D4, bitRead(data,4));
-  digitalWrite(D5, bitRead(data,5));
-  digitalWrite(D6, bitRead(data,6));
-  digitalWrite(D7, bitRead(data,7));
+  for(int i = 0; i<8 ; i++){
+    digitalWrite(Dout, bitRead(data,i)); 
+    cicloClock(); 
+  }
+}
+
+void cicloClock(){
   digitalWrite(W_CLK, HIGH);
+  delayMicroseconds(1);
   digitalWrite(W_CLK, LOW);
 }
 
